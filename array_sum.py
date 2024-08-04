3a521e6f2aa6c64a9403e35d9a774271bfdb65a7
student-om <155445191+student-om@users.noreply.github.com>
2024-08-04 19:40:21 +0530

    04-08-2024

---
+++ array_sum.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    sum=0
+    for i in ar:
+        sum+=i
+       
+    return sum    
